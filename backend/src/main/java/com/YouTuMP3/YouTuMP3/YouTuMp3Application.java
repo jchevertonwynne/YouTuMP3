74da43c813260d9b32007cf50c178e55ae53e16f
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2020-04-26 13:14:25 +0100

    added hello world

--- backend/src/main/java/com/YouTuMP3/YouTuMP3/YouTuMp3Application.java
+++ backend/src/main/java/com/YouTuMP3/YouTuMP3/YouTuMp3Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YouTuMp3Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(YouTuMp3Application.class, args);
 	}
-
 }
