f07869d866319888575b1bfceb9917421297e665
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2020-04-26 12:47:09 +0100

    initial commit

---
+++ backend/src/main/java/com/YouTuMP3/YouTuMP3/YouTuMp3Application.java
@@ -0,0 +1,13 @@
+package com.YouTuMP3.YouTuMP3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTuMp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTuMp3Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/YouTuMP3/YouTuMP3/YouTuMp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YouTuMP3.YouTuMP3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTuMp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
